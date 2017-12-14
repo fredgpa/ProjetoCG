ce4cda781125a87e8fd7470c0fac882d3bba4617
Fred Azzolini <fred_gpa@hotmail.com>
2017-12-13 22:49:49 -0200

    primeiro commit, carro e pista

---
+++ Assets/Scripts/Controllers/car_controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car_controller : MonoBehaviour {
+
+	public object car;
+	// Use this for initialization
+	void Start () {
+		car = GameObject.FindGameObjectWithTag ("Car");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate (Vector3.up * 5 * Time.deltaTime, Space.World);
+		} 
+		else {
+			transform.Translate (Vector3.up * 0 * Time.deltaTime, Space.World);
+		}
+	}
+}
